cf7fe4878fbfbdfe91b54999dcd3152981e7f5c6
hcolmanccc <156472786+hcolmanccc@users.noreply.github.com>
2024-02-09 20:57:25 +0000

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -1,9 +1,9 @@
 var ptx_lunr_search_style = "textbook";
 var ptx_lunr_docs = [
 {
-  "id": "frontmatter-2",
+  "id": "colophon-1",
   "level": "1",
-  "url": "frontmatter-2.html",
+  "url": "colophon-1.html",
   "type": "Colophon",
   "number": "",
   "title": "Colophon",
@@ -16,12 +16,12 @@ var ptx_lunr_docs = [
   "type": "Section",
   "number": "1.1",
   "title": "First section",
-  "body": " First section  Text of section. For example \\begin{tikzpicture} \\begin{axis}[ axis lines = center, xlabel = \\(x\\), ylabel = {\\(y\\)}, domain = -1:1, ymin = -1, ymax = 2, ] \\addplot [] {x^2}; \\end{axis} \\end{tikzpicture}  "
+  "body": " First section  Text of section. For example   Create a Set  Create a variable. Do not use reserved word or white space for a variable name. After the variable, we use an equal sign to assign (store) value. Define a set by listing the contents separated by commas in between a pair of curly braces.     Intersection  Intersection of sets     "
 },
 {
-  "id": "backmatter-2",
+  "id": "colophon-2",
   "level": "1",
-  "url": "backmatter-2.html",
+  "url": "colophon-2.html",
   "type": "Colophon",
   "number": "",
   "title": "Colophon",
@@ -33,7 +33,6 @@ var ptx_lunr_idx = lunr(function () {
   this.ref('id')
   this.field('title')
   this.field('body')
-  this.metadataWhitelist = ['position']
 
   ptx_lunr_docs.forEach(function (doc) {
     this.add(doc)
